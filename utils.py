3f344cc24e4d1bfaec747fd864f88094a96a0c47
yoavharlap <yoavharlap1@gmail.com>
2023-06-04 14:55:04 +0300

    hi print

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import torch
 
 
 def pdist(vectors):
+    print("hi")
     distance_matrix = -2 * vectors.mm(torch.t(vectors)) + vectors.pow(2).sum(dim=1).view(1, -1) + vectors.pow(2).sum(
         dim=1).view(-1, 1)
     return distance_matrix
